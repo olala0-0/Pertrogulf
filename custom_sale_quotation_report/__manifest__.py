474ad0e3f6755ea1b808fe399dcd92d878c27db9
Dalal-Palmate <dalal@palmate.com>
2026-08-01 23:49:01 +0400

    Sales qutation PDF report - marine

--- custom_sale_quotation_report/__manifest__.py
+++ custom_sale_quotation_report/__manifest__.py
@@ -23,6 +23,7 @@
         'report/template_petrogulf_automotive.xml',
         'report/template_petrogulf_aviation.xml',
         'report/template_toll_blending.xml',
+        'report/template_petrogulf_marine.xml',
         'report/header_labels.xml',
     ],
     'installable': True,
